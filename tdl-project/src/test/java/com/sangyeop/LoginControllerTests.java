718b0716924de94f8c8e697f687f22f3a9cf317b
hagome <gktkdk950816@naver.com>
2019-04-01 00:43:02 +0900

    컨트롤러 테스트 이름변경

--- tdl-project/src/test/java/com/sangyeop/LoginControllerTests.java
+++ tdl-project/src/test/java/com/sangyeop/LoginControllerTests.java
@@ -1,18 +1,14 @@
 package com.sangyeop;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sangyeop.controller.LoginController;
-import com.sangyeop.domain.UserRequestDto;
-import com.sangyeop.domain.UserRole;
+import com.sangyeop.domain.User;
+import com.sangyeop.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -21,13 +17,15 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import static org.junit.Assert.*;
+
 /**
  * @author hagome
  * @since 2019-03-30
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = BootApplication.class)
-public class BootAppControllerTests {
+@SpringBootTest
+public class LoginControllerTests {
 
     @Autowired
     ObjectMapper objectMapper;
@@ -35,6 +33,9 @@ public class BootAppControllerTests {
     @Autowired
     private WebApplicationContext wac;
     private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Before
     public void setUp() {
@@ -60,15 +61,17 @@ public class BootAppControllerTests {
     /* 회원가입 페이지 POST 테스팅 */
     @Test
     public void testPostSign_up() throws Exception {
-        UserRequestDto userRequestDto = new UserRequestDto();
-        userRequestDto.setId("test");
-        userRequestDto.setPw("password");
-        userRequestDto.setEmail("Test@gmail.com");
+        User user = new User();
+        user.setId("test");
+        user.setPw("password");
+        user.setEmail("Test@gmail.com");
 
         this.mockMvc.perform(post("/login/sign_up")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(userRequestDto)))
+                .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isCreated());
+
+        assertNotNull(userRepository.findById("test"));
     }
     
 }
